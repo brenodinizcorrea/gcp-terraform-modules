45657d6628ea10ada470baab26046c8e98326a69
brenodinizcorrea <brenodinizcorrea@gmail.com>
2025-07-16 15:06:21 -0300

    main-first

--- modules/bigquery-dataset/dataset.tf
+++ modules/bigquery-dataset/dataset.tf
@@ -2,4 +2,4 @@ resource "google_bigquery_dataset" "dataset" {
   dataset_id                  = "test_dataset"  
   location                    = "us-central1"
   project                     = "personal-finance-445622"
-} 
+}
